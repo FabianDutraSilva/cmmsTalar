5314d45ecf4cf877c4b1704b50bfc5faa612becd
Fabian Dutra Silva <fabiandutrasilva@gmail.com>
2023-06-26 16:13:34 -0300

    CMMSTalar: edit settings.py for fixing static files bug

--- cheep/settings.py
+++ cheep/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,11 +25,11 @@ SECRET_KEY = 'django-insecure-5&u$c@70ph*qti5ty^f!5_=e=+ci&x#b)&ydua5azkly7&dwk3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cmmstalar.pythonanywhere.com']
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-    ]
+#STATICFILES_DIRS = [
+#    BASE_DIR / 'static'
+#    ]
 
 # Application definition
 
@@ -122,7 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # or another location that isn't in STATICFILES_DIRS
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
